3dfb37cd9520cb9a19df244939763a2677a839cf
SalmaFabel <60557679+SalmaFabel@users.noreply.github.com>
2020-03-19 16:38:18 -0700

    T06.- Libreria App 2

---
+++ src/app/libros/libros-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LibrosPage } from './libros.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LibrosPage,
+    children:[
+      {
+        path:'sugeridos',
+        children:[
+          {
+            path: '',
+            loadChildren: () => import('./sugeridos/sugeridos.module').then( m => m.SugeridosPageModule)
+          },
+          {
+            path: 'libroId',
+            loadChildren: () => import('./sugeridos/sugerido-detalle/sugerido-detalle.module').then( m => m.SugeridoDetallePageModule)
+          },
+
+        ]
+      },
+      {
+        path:'categorias',
+        children:[{
+
+          path:'',
+          loadChildren: () => import('./categorias/categorias.module').then( m => m.CategoriasPageModule)
+        },
+        {
+          path:'new',
+          loadChildren: () => import('./categorias/nueva-categoria/nueva-categoria.module').then( m => m.NuevaCategoriaPageModule)
+        }
+
+        ]
+      }
+    ]
+  },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LibrosPageRoutingModule {}
